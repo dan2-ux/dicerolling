7545017b6d99615ba1b77d8341bb1600d737c39e
dan2-ux <danhlanhat9@gmail.com>
2024-09-21 17:40:55 +0700

    dice.js

--- dice.js
+++ dice.js
@@ -5,7 +5,7 @@ function Rolling(){
     for(let i = 0; i < inputnum; i++){
         let so = Math.floor(Math.random()*6)+1
         number.push(so)
-        dice.push(` <img src="/rollDice/diceimage/dice-${so}.svg">`)
+        dice.push(` <img src="/diceimage/dice-${so}.svg">`)
     }
     document.getElementById("numberDisplayer").textContent =   `Dice : ${number.join(", ")}`
     document.getElementById("diceDisplayer").innerHTML = dice
