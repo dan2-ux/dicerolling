2cfec8df7e1f481e9f0f9861300ec1b51b61a737
dan2-ux <danhlanhat9@gmail.com>
2024-09-21 17:38:38 +0700

    Add files via upload

---
+++ rollDice/dice.js
@@ -0,0 +1,12 @@
+function Rolling(){
+    inputnum = document.getElementById("inputnum").value
+    let number = []
+    let dice = []
+    for(let i = 0; i < inputnum; i++){
+        let so = Math.floor(Math.random()*6)+1
+        number.push(so)
+        dice.push(` <img src="/rollDice/diceimage/dice-${so}.svg">`)
+    }
+    document.getElementById("numberDisplayer").textContent =   `Dice : ${number.join(", ")}`
+    document.getElementById("diceDisplayer").innerHTML = dice
+}
